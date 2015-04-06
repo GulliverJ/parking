6a5c530b65d9ef3a2d452bb8a65c3e9f989a3472
GulliverJ <gully.johnson@gmail.com>
2015-04-07 00:24:28 +0100

    Added index.php with initial page

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+<h1>Orange Parking</h1>
+<p>This page should display an embedded Google map with pointers indicating parking bays, along with their data (whether it's legal to park there, whether it's a disabled parking space, how long the maximum stay is...)</p>
+</body>
+</html>
