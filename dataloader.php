7eba827831a206ad7b356ef1c9fb4e07579dd55c
GulliverJ <gully.johnson@gmail.com>
2015-04-14 08:19:45 +0100

    Having fun learning JS

--- dataloader.php
+++ dataloader.php
@@ -20,8 +20,8 @@
       echo '"occupied": ' . ($row[0] ? 'Yes' : 'No') . ',';
       echo '"duration": {$row[1]},';
       echo '"remaining": {$row[2]},';
-      echo '"legal": ' . ($row[3] ? 'Yes' : 'No'} . ',';
-      echo '"restricted": ' . ($row[4] ? 'Yes' : 'No'} . ',';
+      echo '"legal": ' . ($row[3] ? 'Yes' : 'No') . ',';
+      echo '"restricted": ' . ($row[4] ? 'Yes' : 'No') . ',';
       echo '"max_stay": {$row[5]},';
       echo '"nearest_unoccupied": {$row[6]}';
       echo '}';
