1023c4cb8106ec9570d77172700fadd0a7b78ad4
vnjk <vnjk@users.noreply.github.com>
2015-04-14 10:26:04 +0100

    changing json.php

--- json.php
+++ json.php
@@ -13,8 +13,8 @@
 			$initial = false;
 			echo '{';
 			echo '"id": ' . $row[0] . ',';
-			echo '"occupied": ' . ($row[1] ? 'true' : 'false'); . ',';
-			echo '"bay": ' . $row[0];// . ',';
+			echo '"occupied": ' . ($row[1] ? 'true' : 'false') . ',';
+			echo '"nearest-unoccupied-bay": ' . $row[2];// . ',';
 			//echo '"max_stay": "' . "\"{$row[3]}\" . '"';
 			/*echo '"start": "' . $row[4] . '",';
 			echo '"end": "' . $row[5]; . '"';*/
