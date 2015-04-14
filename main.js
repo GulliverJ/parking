4a51b2236a9c316f5b4b230c35a88f975d7a3056
GulliverJ <gully.johnson@gmail.com>
2015-04-14 09:39:54 +0100

    Testing sidebar operation with javascript

--- main.js
+++ main.js
@@ -20,32 +20,31 @@ function makeMap(div) {
 			});
 			markers[id] = marker;
 			infoWindows[id] = infoWindow;
-			google.maps.event.addListener(marker, 'click', function() {
-
+			google.maps.event.addListener(marker, 'click', function() {}
 				console.log(id);
-					
-					$.getJSON('dataloader.php', function(data) {
-						$.each(data, function(key, value) {
-							console.log(value.occupied);
+				
+				$.getJSON('dataloader.php', function(data) {
+					console.log(id);
+					$.each(data, function(key, value) {
+						console.log(id);
+						console.log(value.occupied);
+						document.getElementById("occupied").innerHTML = 'Occupied: ' + value.occupied;
+						document.getElementById("duration").innerHTML = 'For: ' + value.duration;
+						//document.getElementByID("remaining").innerHTML = 'Time remaining: ' + value.remaining;
+						//document.getElementByID("legal").innerHTML = 'Legally parked: ' + value.legal;
+						document.getElementById("restricted").innerHTML = 'Restricted: ' + value.restricted;
+						document.getElementById("max_stay").innerHTML = 'Maximum stay: ' + value.max_stay;
+						document.getElementById("nearest_unoccupied").innerHTML = 'Nearest unoccupied bay: ' + value.nearest_unoccupied;
 
-							document.getElementByID("occupied").innerHTML = 'Occupied: ' + value.occupied;
-							document.getElementByID("duration").innerHTML = 'For: ' + value.duration;
-							//document.getElementByID("remaining").innerHTML = 'Time remaining: ' + value.remaining;
-							//document.getElementByID("legal").innerHTML = 'Legally parked: ' + value.legal;
-							document.getElementByID("restricted").innerHTML = 'Restricted: ' + value.restricted;
-							document.getElementByID("max_stay").innerHTML = 'Maximum stay: ' + value.max_stay;
-							document.getElementByID("nearest_unoccupied").innerHTML = 'Nearest unoccupied bay: ' + value.nearest_unoccupied;
-
-						});
 					});
-				infoWindow.open(map, marker);
-				// Ideally want to initiate code to get the values and display them. right?
-			});
+				});
+			infoWindow.open(map, marker);
+			// Ideally want to initiate code to get the values and display them. right?
+		});
 			return marker;
 		},
 		updateMarker: function(id, colour, content) {
-			//markers[id].setIcon('http://maps.google.com/mapfiles/ms/icons/' + colour + '-dot.png');
-			markers[id].setIcon('img/testicon.png');
+			markers[id].setIcon('http://maps.google.com/mapfiles/ms/icons/' + colour + '-dot.png');
 			infoWindows[id].setContent(content);
 		}
 
