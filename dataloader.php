d77e450a171442f8661fde30376f34dffbfbf59a
GulliverJ <gully.johnson@gmail.com>
2015-04-14 09:10:18 +0100

    Testing sidebar operation with javascript

--- dataloader.php
+++ dataloader.php
@@ -5,7 +5,7 @@
   ini_set('display_errors','On');
   error_reporting(E_ALL);
   //$id = isset( $_GET['id'] ) ? $_GET['id'] : "10";
-  $query = 'SELECT occupied, state_time, time_remaining, legally_occupied, restricted, max_stay, nearest_unoccupied_bay FROM bay_data_view WHERE bay_id = 10';
+  $query = 'SELECT occupied, state_time, restricted, max_stay, nearest_unoccupied_bay FROM bay_data_view WHERE bay_id = 10';
   $result = mysql_query($query);
   echo '[';
   if (is_resource($result) && mysql_num_rows($result)) {
@@ -18,11 +18,11 @@
       echo '{';
       echo '"occupied": ' . ($row[0] ? 'Yes' : 'No') . ',';
       echo '"duration": {$row[1]},';
-      echo '"remaining": {$row[2]},';
-      echo '"legal": ' . ($row[3] ? 'Yes' : 'No') . ',';
-      echo '"restricted": ' . ($row[4] ? 'Yes' : 'No') . ',';
-      echo '"max_stay": {$row[5]},';
-      echo '"nearest_unoccupied": {$row[6]}';
+      //echo '"remaining": {$row[2]},';
+      //echo '"legal": ' . ($row[3] ? 'Yes' : 'No') . ',';
+      echo '"restricted": ' . ($row[2] ? 'Yes' : 'No') . ',';
+      echo '"max_stay": {$row[3]},';
+      echo '"nearest_unoccupied": {$row[4]}';
       echo '}';
     }
   }
