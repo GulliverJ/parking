f86ef6787ccb019e3d0f01987dde68f4176caf82
GulliverJ <gully.johnson@gmail.com>
2015-04-14 07:59:19 +0100

    Testing new functionality with the sidebar

--- dataloader.php
+++ dataloader.php
@@ -2,6 +2,9 @@
   require 'connect.php';
   header('Content-Type: application/json');
 
+  ini_set('display_errors','On');
+  error_reporting(E_ALL);
+
   $id = $_GET['id'];
   $query = 'SELECT occupied, state_time, time_remaining, legally_occupied, restricted, max_stay, nearest_unoccupied_bay FROM bay_data_view WHERE bay_id = {$id}';
   $result = mysql_query($query);
