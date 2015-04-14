47cee0c2ee1966d847701d8131800de1f31da11d
GulliverJ <gully.johnson@gmail.com>
2015-04-14 09:29:27 +0100

    Testing sidebar operation with javascript

--- dataloader.php
+++ dataloader.php
@@ -5,8 +5,8 @@
   ini_set('display_errors','On');
   error_reporting(E_ALL);
   //$id = isset( $_GET['id'] ) ? $_GET['id'] : "10";
-  //$query = 'SELECT occupied, state_time, restricted, max_stay, nearest_unoccupied_bay FROM bay_data_view WHERE bay_id = 10';
-  //$result = mysql_query($query);
+  $query = 'SELECT occupied, state_time, restricted, max_stay, nearest_unoccupied_bay FROM bay_data_view WHERE bay_id = 10';
+  $result = mysql_query($query);
   echo '[';
   
   if (is_resource($result) && mysql_num_rows($result)) {
@@ -17,11 +17,11 @@
       }
       $initial = false;
       echo '{';
-      echo '"occupied": ' . ($row[0] ? 'Yes' : 'No') . ',';
+      echo '"occupied": {$row[0]},';
       echo '"duration": {$row[1]},';
       //echo '"remaining": {$row[2]},';
       //echo '"legal": ' . ($row[3] ? 'Yes' : 'No') . ',';
-      echo '"restricted": ' . ($row[2] ? 'Yes' : 'No') . ',';
+      echo '"restricted": {$row[2]},';
       echo '"max_stay": {$row[3]},';
       echo '"nearest_unoccupied": {$row[4]}';
       echo '}';
