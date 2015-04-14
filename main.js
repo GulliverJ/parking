febb2bef29f12dba1cc95be120d69686fe224daa
GulliverJ <gully.johnson@gmail.com>
2015-04-14 22:20:31 +0100

    Working on last bit of UI

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function makeMap(div) {
 						document.getElementById("sensorid").innerHTML = id;
 						document.getElementById("occupied").innerHTML = value.occupied;
 						document.getElementById("duration").innerHTML = parseInt(value.duration/3600) + " hours, " + parseInt((value.duration/60)%3600) + " mins, " + value.duration%60 = "seconds";
-						document.getElementById("max_stay").innerHTML = parseInt(value.max_stay/3600) + " hours, " + parseInt((value.max_stay/60)%3600) + " mins, " + value.max_stay%60 = "seconds"
+						document.getElementById("max_stay").innerHTML = parseInt(value.max_stay/3600) + " hours, " + parseInt((value.max_stay/60)%3600) + " mins, " + value.max_stay%60 = "seconds";
 						document.getElementById("remaining").innerHTML = value.remaining;
 						document.getElementById("restricted").innerHTML = value.restricted;
 						document.getElementById("legal").innerHTML = 'Legally parked: ' + value.legal;
