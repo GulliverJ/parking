f8ec9c8ef7df53658f24cb594ccf2c03e2301b6a
GulliverJ <gully.johnson@gmail.com>
2015-04-14 13:11:59 +0100

    Testing ideas/learning JS

--- dataloader.php
+++ dataloader.php
@@ -12,17 +12,7 @@
     $id = isset( $_GET['id'] ) ? $_GET['id'] : "0"; // Application field isn't required
 
     // Retrieve the current max sensor_id belonging to this user
-    $sql_statement = $connection->prepare('SELECT v.occupied, v.state_time, v.time_remaining, v.max_stay,
-                                                  v.restricted, v.legally_occupied, v.nearest_unoccupied_bay,
-                                                  r.start, r.end, r.hourly_charge
-                                           FROM bay_data_view v 
-                                           INNER JOIN bays b 
-                                           ON v.bay_id = b.bay_id 
-                                           INNER JOIN restrictions r 
-                                           ON b.restriction_id = r.restriction_id 
-                                           WHERE v.bay_id = 10');
-
-
+    $sql_statement = $connection->prepare('SELECT v.occupied, v.state_time, v.time_remaining, v.max_stay, v.restricted, v.legally_occupied, v.nearest_unoccupied_bay, r.start, r.end, r.hourly_charge FROM bay_data_view v INNER JOIN bays b ON v.bay_id = b.bay_id INNER JOIN restrictions r ON b.restriction_id = r.restriction_id WHERE v.bay_id = ?');
     $sql_statement->execute( array($id));
 
     $results = $sql_statement->fetchAll();
@@ -34,9 +24,9 @@
     foreach($results as $row) {
       echo '{';
       echo '"occupied": ' . ($row[0] ? 'Occupied' : 'Available') . ',';
-      echo '"duration": "' . $row[1] . '"';
-      echo '"remaining": "' . $row[2] . '"';
-      echo '"max_stay": "' . $row[3] . '"';
+      echo '"duration": "' . $row[1] . '",';
+      echo '"remaining": "' . $row[2] . '",';
+      echo '"max_stay": "' . $row[3] . '",';
       echo '"restricted": ' . ($row[4] ? 'Restrictions in place' : 'Free parking') . ',';
       echo '"legal": ' . ($row[5] ? 'true' : 'false') . ',';
       echo '"nearest_available": "' . $row[6] . '",';
