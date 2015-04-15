dbf9691760301d92d092a03896c30b358932d5ab
GulliverJ <gully.johnson@gmail.com>
2015-04-15 23:44:50 +0100

    Updating

--- main.js
+++ main.js
@@ -25,7 +25,9 @@ function makeMap(div) {
 						document.getElementById("sensorid").innerHTML = id;
 						document.getElementById("occupied").innerHTML = (value.occupied == 'Occupied') ? value.occupied : '<span style="color: #0b1">' + value.occupied + '</span>';
 						var durationTime = parseTime(value.duration);
-						document.getElementById("duration").innerHTML = durationTime.hours + ' hours ' + durationTime.mins + ' mins ' + durationTime.secs + ' secs';
+						document.getElementById("duration").innerHTML = (durationTime.hours ? durationTime.hours + ' hours ' : '')
+																													+ (durationTime.mins ? durationTime.mins + ' mins' : '')
+																													+ (durationTime.secs ? durationTime.secs + ' secs': '');
 						document.getElementById("max_stay").innerHTML = parseInt(value.max_stay/3600) + " hours, " + parseInt((value.max_stay/60)%60) + " mins";
 						document.getElementById("remaining").innerHTML = value.remaining;
 						document.getElementById("restricted").innerHTML = value.restricted;
