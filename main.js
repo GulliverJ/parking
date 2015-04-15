f01477e6658ca13cfe91ac0647eae98bcf5ce3ec
GulliverJ <gully.johnson@gmail.com>
2015-04-15 22:16:12 +0100

    Updates part 1

--- main.js
+++ main.js
@@ -23,13 +23,14 @@ function makeMap(div) {
 				$.getJSON('dataloader.php?id=' + id, function(data) {
 					$.each(data, function(key, value) {
 						document.getElementById("sensorid").innerHTML = id;
-						document.getElementById("occupied").innerHTML = (value.occupied == 'Occupied') ? value.occupied : '<span style="color: #ob1>' + value.occupied + '</span>';
+						document.getElementById("occupied").innerHTML = (value.occupied == 'Occupied') ? value.occupied : '<span style="color: #ob1">' + value.occupied + '</span>';
 						document.getElementById("duration").innerHTML = parseInt(value.duration/3600) + " hours, " + parseInt((value.duration/60)%60) + " mins, " + value.duration%60 + " seconds";
 						document.getElementById("max_stay").innerHTML = parseInt(value.max_stay/3600) + " hours, " + parseInt((value.max_stay/60)%60) + " mins";
 						document.getElementById("remaining").innerHTML = value.remaining;
 						document.getElementById("restricted").innerHTML = value.restricted;
 						document.getElementById("legal").innerHTML = value.legal;
 						document.getElementById("nearest_available").innerHTML = value.nearest_available;
+						console.log(value.rest_start);
 						document.getElementById("restrictions").innerHTML = 'Enforced between ' + value.rest_start + ' and ' + value.rest_end;
 						document.getElementById("charge").innerHTML = value.charge;
 						map.panTo(markers[value.nearest_available].getPosition());
@@ -43,7 +44,7 @@ function makeMap(div) {
 		},
 		updateMarker: function(id, type, content) {
 			markers[id].setIcon('img/bayicon-' + type + '.png');
-			var layer = (type == 'occ') ? 1 : 2;
+			var layer = (type == 'occ2') ? 1 : 2;
 			markers[id].setZIndex(layer);
 			info[id] = content;
 		}
