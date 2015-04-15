9f3eb8d9e01717dae9675ce53649667a5f2fb5ec
GulliverJ <gully.johnson@gmail.com>
2015-04-15 23:19:25 +0100

    Updates part 1

--- dataloader.php
+++ dataloader.php
@@ -8,10 +8,8 @@
     $connection = new PDO( "mysql:host=$server_name;dbname=$db_name", $db_username, $db_password );
     $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
+    $id = isset( $_GET['id'] ) ? $_GET['id'] : "0";
 
-    $id = isset( $_GET['id'] ) ? $_GET['id'] : "0"; // Application field isn't required
-
-    // Retrieve the current max sensor_id belonging to this user
     $sql_statement = $connection->prepare('SELECT v.occupied, v.state_time, v.time_remaining, v.max_stay, v.restricted, v.legally_occupied, v.nearest_unoccupied_bay, r.start, r.end, r.hourly_charge FROM bay_data_view v INNER JOIN bays b ON v.bay_id = b.bay_id INNER JOIN restrictions r ON b.restriction_id = r.restriction_id WHERE v.bay_id = ?');
     $sql_statement->execute( array($id));
 
@@ -24,69 +22,16 @@
     foreach($results as $row) {
       echo '{';
       echo '"occupied": "' . ($row[0] ? 'Occupied' : 'Available') . '",';
-      echo '"duration": "' . $row[1] . '",';
-      echo '"remaining": "' . $row[2] . '",';
+      echo '"duration": "' . $row[9] . '",';
+      echo '"remaining": "' . $row[7] . '",';
       echo '"max_stay": "' . $row[3] . '",';
       echo '"restricted": "' . ($row[4] ? 'Restrictions in place' : 'No - Free parking') . '",';
-      echo '"legal": "' . ($row[5] ? 'Yes' : 'No') . '",';
+      echo '"legal": "' . (!$row[5] ? 'No' : 'Yes') . '",';
       echo '"nearest_available": "' . $row[6] . '",';
-      echo '"rest_start": "' . $row[7] . '",';
-      echo '"rest_end": "' . $row[8] . '",';
+      echo '"rstart": "' . $row[7] . '",';
+      echo '"rend": "' . $row[8] . '",';
       echo '"charge": "' . $row[9] . '"';
       echo '}';
     }
     echo ']';
-
-
-/*
-  require 'connect.php';
-  header('Content-Type: application/json');
-  $id = 10;
-  $query = 'SELECT occupied, restricted FROM bay_data_view WHERE bay_id = {$id}';   
-  $result = mysql_query($query);
-  echo '[';
-  if (is_resource($result) && mysql_num_rows($result)) {
-    $initial = true;
-    while ($row = mysql_fetch_row($result)) {
-      if (!$initial) {
-        echo ',';
-      }
-      $initial = false;
-      echo '{';
-      echo '"occupied": ' . ($row[0] ? 'true' : 'false') . ',';
-      echo '"restricted": "' . $row[1] . '"';
-      echo '}';
-    }
-  }
-  echo ']';
-/*
-
-  require 'connect.php';
-  header('Content-Type: application/json');
-
-  //$id = isset( $_GET['id'] ) ? $_GET['id'] : "10";
-  $query = 'SELECT occupied FROM bay_data WHERE bay_id = 10';
-  $result = mysql_query($query);
-  echo '[';
-  
-  if (is_resource($result) && mysql_num_rows($result)) {
-    $initial = true;
-    while ($row = mysql_fetch_row($result)) {
-      if (!$initial) {
-        echo ',';
-      }
-      $initial = false;
-      echo '{';
-      echo '"occupied": {$row[0]}';
-      //echo '"duration": {$row[1]},';
-      //echo '"remaining": {$row[2]},';
-      //echo '"legal": ' . ($row[3] ? 'Yes' : 'No') . ',';
-      //echo '"restricted": {$row[2]},';
-      //echo '"max_stay": {$row[3]},';
-      //echo '"nearest_unoccupied": {$row[4]}';
-      echo '}';
-    }
-  }
-  echo ']';
-*/
 ?>
