f1807e89a7c08facfd8f26114e1cfa2e1fb3de52
GulliverJ <gully.johnson@gmail.com>
2015-04-16 13:13:51 +0100

    Final updates to parking site

--- getnew.php
+++ getnew.php
@@ -2,6 +2,7 @@
   error_reporting(E_ALL);
   ini_set("log_errors", 1);
   ini_set('display_errors', 1);
+  date_default_timezone_set('Europe/London');
 
   header('Content-Type: application/json');
   $server_name = "localhost";
