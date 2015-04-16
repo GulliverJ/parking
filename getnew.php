706ab9924f0cc0e075d2191eae1357aa35b38407
GulliverJ <gully.johnson@gmail.com>
2015-04-16 04:29:24 +0100

    Finishing final version of parking site

--- getnew.php
+++ getnew.php
@@ -15,9 +15,11 @@
 
     $time = isset( $_GET['time'] ) ? $_GET['time'] : "2015-01-01 00:00:00";
     $newtime = date("Y-m-d H:i:s", $time);
+    $lastcheck = mktime(date("s")-3, date("i"), date("H"), date("d"), date("m"), date("Y"));
+    $testdate = date("Y-m-d H:i:s"); 
 
-    $sql_statement = $connection->prepare("SELECT b.bay_id, v.occupied, nearest_unoccupied_bay, r.max_stay, start, end, legally_occupied, d.state_timespan FROM bay_data_view v INNER JOIN bays b ON v.bay_id = b.bay_id INNER JOIN restrictions r ON b.restriction_id = r.restriction_id INNER JOIN bay_data d ON d.bay_id = v.bay_id WHERE d.state_timespan > '?' ORDER BY v.bay_id asc");
-    $sql_statement->execute( array($newtime));
+    $sql_statement = $connection->prepare("SELECT b.bay_id, v.occupied, nearest_unoccupied_bay, r.max_stay, start, end, legally_occupied, d.state_timespan FROM bay_data_view v INNER JOIN bays b ON v.bay_id = b.bay_id INNER JOIN restrictions r ON b.restriction_id = r.restriction_id INNER JOIN bay_data d ON d.bay_id = v.bay_id WHERE d.state_timespan > ? ORDER BY v.bay_id asc");
+    $sql_statement->execute( array($lastcheck));
 
     $results = $sql_statement->fetchAll();
 
