00431ed52ac5db440a4c186866b2094e207ffb3b
GulliverJ <gully.johnson@gmail.com>
2015-04-16 13:11:51 +0100

    Final updates to parking site

--- getnew.php
+++ getnew.php
@@ -14,7 +14,7 @@
     $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
     $time = isset( $_GET['time'] ) ? $_GET['time'] : "2015-01-01 00:00:00";
-    $newtime = date("Y-m-d H:i:s", $time);
+    $newtime = date("Y-m-d H:i:s", strtotime('-4 seconds'));
     //$lastcheck = mktime(date("s")-3, date("i"), date("H"), date("d"), date("m"), date("Y"));
     //$testdate = "2015-04-16 12:15:00"; 
 /*
