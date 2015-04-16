19defed2bd8f7f5d6fec56301778bb811f55aa1d
GulliverJ <gully.johnson@gmail.com>
2015-04-16 14:16:18 +0100

    Final updates to parking site

--- getnew.php
+++ getnew.php
@@ -2,23 +2,7 @@
   error_reporting(E_ALL);
   ini_set("log_errors", 1);
   ini_set('display_errors', 1);
-
-
   date_default_timezone_set('Europe/London');
-/*
-  header('Content-Type: application/json');
-  $server_name = "localhost";
-  $db_name = "orangeparking";
-  $db_username = "parking";
-  $db_password = "parking";  
-
-  try {
-    $connection = new PDO( "mysql:host=$server_name;dbname=$db_name", $db_username, $db_password );
-    $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-    $time = isset( $_GET['time'] ) ? $_GET['time'] : "2015-01-01 00:00:00";
-    $newtime = date("Y-m-d H:i:s", strtotime('-4 seconds'));
-*/
 
   header('Content-Type: application/json');
   $server_name = "localhost";
@@ -30,30 +14,11 @@
     $connection = new PDO( "mysql:host=$server_name;dbname=$db_name", $db_username, $db_password );
     $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
+    // Set a date to check from - the AJAX should update very 3 seconds, so let's check from the past 4 seconds to be safe
     $init = isset( $_GET['initial'] ) ? $_GET['initial'] : 1;
-    $newtime = $init == 1 ? date("Y-m-d H:i:s", mktime(0, 0, 0, 4, 10, 2015)) : date("Y-m-d H:i:s", mktime(date("H"), date("i"), date("s")-4, date("m"), date("d"), date("Y"))); //- 4 seconds
-    //$seconds = intval($time / 1000);
-    //$newtime = date("Y-m-d H:i:s", $seconds);
-    //$lastcheck = mktime(date("s")-3, date("i"), date("H"), date("d"), date("m"), date("Y"));
-    //$testdate = "2015-04-16 12:15:00"; 
-/*
-    $sql_statement = $connection->prepare("SELECT bay_id FROM bay_data WHERE state_timespan >= ?");
-    $sql_statement->execute( array($testdate) );
-    $results = $sql_statement->fetchAll();
-    $numrows = count($results);
-    $inclause = '(';
-    $count = 0;
-    foreach($results as $row) {
-      $count++;
-      $inclause += '' . $row[0] . '';
-      if($count < $numrows) {
-        $inclause += ', ';
-      }
-    }
-    $inclause += ')';
+    $newtime = $init == 1 ? date("Y-m-d H:i:s", mktime(0, 0, 0, 4, 10, 2015)) : date("Y-m-d H:i:s", mktime(date("H"), date("i"), date("s")-4, date("m"), date("d"), date("Y")));
 
-    $tester = '(1, 2, 3)';
-*/
+    // Statement will return the data for rows ONLY if they have updated since the last check
     $sql_statement = $connection->prepare("SELECT b.bay_id, v.occupied, nearest_unoccupied_bay, r.max_stay, start, end, legally_occupied FROM bay_data_view v INNER JOIN bays b ON v.bay_id = b.bay_id INNER JOIN restrictions r ON b.restriction_id = r.restriction_id INNER JOIN bay_data d ON d.bay_id = v.bay_id WHERE d.state_timespan >= ? ORDER BY v.bay_id asc");
     $sql_statement->execute( array($newtime) );
 
