7ef1b123229db708ece8365602803786206b9fbe
GulliverJ <gully.johnson@gmail.com>
2015-04-18 22:10:24 +0100

    Minor title update!

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 	<div class="sidebar">
 		<div class="header">
 			<a href="http://victokoh.cs.ucl.ac.uk"><img src="img/main-button.png" class="returnlink"></a>
-			<img src="img/parking_title.png" style="display: inline; margin-left: 88px;">
+			<img src="img/parking_title.png" style="display: inline; margin-left: 82px;">
 		</div>
 		<div class="results">
 			<div class="resultshead">
